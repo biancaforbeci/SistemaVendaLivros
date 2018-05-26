ae4a2407317b2ae514a40a7cb1b52e585139a88e
Bianca Forbeci <biaformic@hotmail.com>
2018-05-26 19:22:08 -0300

    Criando projeto, inserindo entity, models (cliente,livro e endereço)

---
+++ SistemaLivros/SistemaLivros/Models/DAL/MeuContexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SistemaLivros.Models.DAL
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<MeuContexto>());
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) { modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); }
+
+    }
+}
