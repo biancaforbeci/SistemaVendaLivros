b1197f5e8c6d37139bdb456b0ad800c4571367ab
Bianca Forbeci <biaformic@hotmail.com>
2018-05-27 10:48:30 -0300

    Criando Views de clientes e livros

--- SistemaLivros/SistemaLivros/Models/Livro.cs
+++ SistemaLivros/SistemaLivros/Models/Livro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace SistemaLivros.Models
     public class Livro
     {
         public int LivroID { get; set; }
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public int Qtd { get; set; }
