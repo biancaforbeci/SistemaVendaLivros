5c0e7c70b34f8793c7d0561f58060c277be3d1aa
Bianca Forbeci <biaformic@hotmail.com>
2018-06-14 17:56:32 -0300

    Arrumando estilo

---
+++ SistemaVendaLivros/SistemaVendaLivros/Controllers/CarrinhoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaVendaLivros.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        // GET: Carrinho
+        public ActionResult View()
+        {
+            //add carrinho
+
+            return View();
+        }
+    }
+}
