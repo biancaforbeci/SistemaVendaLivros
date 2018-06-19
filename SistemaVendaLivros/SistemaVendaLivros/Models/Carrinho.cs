60a562646fb4e7e307606bbee25a0794d7f15fe8
bianca forbeci michaloskei <bianca.michaloskei@lactec.org.br>
2018-06-19 13:31:12 -0300

    Adicionando model carrinho

---
+++ SistemaVendaLivros/SistemaVendaLivros/Models/Carrinho.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaVendaLivros.Models
+{
+    public class Carrinho
+    {
+        public int CarrinhoID { get; set; }
+        public int LivroID { get; set; }
+        public int ClienteID { get; set; }
+    }
+}
